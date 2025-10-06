0d82b84a16646c0ecd8d127a7e08cadc227a9c03
luizpaulo2005 <luizpaulorodrigues2005@gmail.com>
2025-10-06 20:58:22 -0300

    feat: add components to home center

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+import { CommunitySection } from '@/components/community-section'
+import { ContentSection } from '@/components/content-section'
+import { CTASection } from '@/components/cta-section'
+import { FeaturesSection } from '@/components/features-section'
+import { Footer } from '@/components/footer'
+import { HeroSection } from '@/components/hero-section'
+import { Navigation } from '@/components/navigation'
+
 export default function Home() {
   return (
-    <main>
-      <div>Hello world!</div>
+    <main className="min-h-screen">
+      <Navigation />
+      <HeroSection />
+      <FeaturesSection />
+      <ContentSection />
+      <CommunitySection />
+      <CTASection />
+      <Footer />
     </main>
-  );
+  )
 }
